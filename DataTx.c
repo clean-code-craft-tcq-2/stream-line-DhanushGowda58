df7539280a322639640efb89817d26bdb92b656c
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-06 22:16:03 +0530

    Create DataTx.c

---
+++ DataTx.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "DataTx.h"
+
+void Transmitting(BatParam_val)
+{
+    printf("{\"Temperature\":%0.2f,\"SOC\":%0.2f}\n",BatParam_val.Temperature,BatParam_val.Soc); // print in the console
+    
+}
+
+int main()
+{
+    for(int i=0;i<20;i++)
+    {
+        BatParam_val.Temperature = (float)i;
+        BatParam_val.Soc = (float)(i*2);
+        
+        Transmitting(BatParam_val);        
+    }
+    return 0;
+}
