998bbd48b2e2119c247cd0de71021ef80f443f12
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-08 08:48:45 +0530

    Update DataTx.c

--- DataTx.c
+++ DataTx.c
@@ -9,10 +9,10 @@ void Transmitting(BatParam_val)
 
 int main()
 {
-    for(int i=0;i<50;i++)
+    for(int snsr_val_read=0;snsr_val_read<50;snsr_val_read++)
     {
-        BatParam_val.Temperature = (float)i;
-        BatParam_val.Soc = (float)(i*2);
+        BatParam_val.Temperature = (float)snsr_val_read;
+        BatParam_val.Soc = (float)(snsr_val_read*2);
         
         Transmitting(BatParam_val);        
     }
