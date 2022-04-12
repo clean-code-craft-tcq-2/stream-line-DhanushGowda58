ec96b8c754ce7ca8aae90f0a8ae79b723eb75a3c
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-12 21:20:43 +0530

    Create Receiver.c

---
+++ Receiver.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+//#include "ReceiveBatteryParameters.h"
+float Temp[50] = {};
+float Soc[50] = {};
+void ReadDataFromConsole(BatteryChargingParameters *batteryParameters)
+{	
+        for (int i = 0; i < 50; i++)
+	{
+		scanf("%f\t%f\n", &Temp[i], &Soc[i]);
+		printf("%f\t%f\n", Temp[i], Soc[i]);
+	}
+}
