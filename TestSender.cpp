2800bc1435a7f3c1d306b61652b5b7902f171705
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-19 17:53:39 +0530

    Create TestSender.cpp

---
+++ TestSender.cpp
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test-framework/catch.hpp"
+#include "DataTx.h"
+
+
+TEST_CASE("Test to check Sender") 
+{
+  
+  //Read the first and last values of the text file and assert they have been properly added to batteryParameters 
+  float expectedBatteryParameters[2][2] = {{3.42, 11.06}, {1.34, 64.52}};
+
+   REQUIRE(BatParam_val[0].Temperature  == expectedBatteryParameters[0][0]);
+   REQUIRE(BatParam_val[0].Soc == expectedBatteryParameters[0][1]);
+
+   REQUIRE(BatParam_val[49].Temperature  == expectedBatteryParameters[1][0]);
+   REQUIRE(BatParam_val[49].Soc == expectedBatteryParameters[1][1]);
+                         
+
+}
