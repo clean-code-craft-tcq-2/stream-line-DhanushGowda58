f53c11aef31a2c8a89ddeea2477a734f1e118942
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-21 17:51:20 +0530

    Update DataRx.c

--- DataRx.c
+++ DataRx.c
@@ -9,6 +9,7 @@ float Temperature[NUMBERS_OF_READINGS] = {};
     for(int i = 0; i < NUMBERS_OF_READINGS ; i++)
    { 
         scanf("%f", &Temperature[i]);
+        printf("Temperature = %f\n",Temperature[i]);
    }
     return 0;
 }
