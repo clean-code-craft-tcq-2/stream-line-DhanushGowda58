d450aa3ac7732be34fed18a0c9f2121283c4841f
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-22 18:15:32 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -7,10 +7,10 @@ int SOCData[NUMBERS_OF_READINGS];
 
 int main()
 {
-string line[256];
+char line[256];
 
 scanf("%255[^\n]%*c",line); // %*c to remove trailing \n
 fgets(line, 256, stdin);
-printf("%s\n", line);
+printf("%c", line);
     
 }
