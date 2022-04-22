66c5650c15f3565ee84fe5fa316410f5ebf774ad
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-22 17:50:53 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -7,9 +7,14 @@ int SOCData[NUMBERS_OF_READINGS];
 int main()
 {
   int TempData;
+  char string [256];
+  printf ("Insert your full address: ");
+  gets (string);
+  printf ("Your address is: %s\n",string);
   for (int i=0;i<50;i++)
   {
     scanf("%d\n",&TempData);
     printf("Temperature data = %d, Temperature data read = %d\n", TemperatureData[i],TempData);
   }
+    return 0;
 }
