ad37b479b0829a0174c913b0c1869e6bf0e6d3cb
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-22 17:59:12 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -7,10 +7,20 @@ int SOCData[NUMBERS_OF_READINGS];
 int main()
 {
   int TempData;
-  char string [256];
-  printf ("Insert your full address: ");
-  fgets (string);
-  printf ("Your address is: %s\n",string);
+char message[N];        /* character array for the message, you can always change the character length */
+int i = 0;          /* loop counter */
+
+printf( "Enter a message: " );
+message[i] = getchar();    /* get the first character */
+while( message[i] != '\n' ){
+    message[++i] = getchar(); /* gets the next character */
+}
+
+printf( "Entered message is:" );
+for( i = 0; i < N; i++ )
+    printf( "%c", message[i] );
+  
+  printf("%c\n",line);
   for (int i=0;i<50;i++)
   {
     scanf("%d\n",&TempData);
