684fa55d0077991f0a92dd4d0f25fa0b67d70fd3
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-22 17:35:33 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -6,7 +6,7 @@ int SOCData[NUMBERS_OF_READINGS];
 void main()
 {
   float TempData;
-  for (i=0;i<50;i++)
+  for (int i=0;i<50;i++)
   {
     scanf("%f\n",&TempData);
     printf("Temperature data = %d", TemperatureData[i]);
