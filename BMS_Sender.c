9d881456d1d3e2cdfeb8388f82831296fcabcd41
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-22 19:25:23 +0530

    Update BMS_Sender.c

--- BMS_Sender.c
+++ BMS_Sender.c
@@ -1,3 +1,21 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include "DataTx.h"
+
+int TemperatureData[NUMBERS_OF_READINGS];
+int SOCData[NUMBERS_OF_READINGS];
+
+void printToConsole()
+{
+	int i;
+  printf("Temperature Data\n");
+	for (i = 0; i < NUMBERS_OF_READINGS; i++)
+	{
+	   printf("Temperature Data = %d\n", i);
+	}
+  printf("SoC Data\n");
+  for (i = 0; i < NUMBERS_OF_READINGS; i++)
+	{
+	   printf("State of Charge Data = %d\n", i/2);
+	}
+}
