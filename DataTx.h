51d7ea04b1ff1d1028f74f7f96975eef0606bbea
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-22 19:32:51 +0530

    Update DataTx.h

--- DataTx.h
+++ DataTx.h
@@ -8,7 +8,7 @@
 extern int TemperatureData[NUMBERS_OF_READINGS];
 extern int SOCData[NUMBERS_OF_READINGS];
 
-int printToConsole(void);
+//int printToConsole(void);
 //bool readDataFromFile(FILE* filePtr, int sensorData[]);
 //bool BMS_DataSender(void);
 //int writeToFile(void);
