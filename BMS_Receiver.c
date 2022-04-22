1d687330da99e3f1380cd0cb6842b77d8cec1773
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-22 19:06:57 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -8,8 +8,7 @@
 int main ()
 {
 	struct BMS bms_input = {.temperature = {0.0, .temp_stat = {0.0, 0.0, 0.0, 0.0, 0.0}}, 
-							.soc = {0.0, .soc_stat = {0.0, 0.0, 0.0, 0.0, 0.0}}, 
-							.chargeRate = {0.0, .CR_stat = {0.0, 0.0, 0.0, 0.0, 0.0}}};
+							.soc = {0.0, .soc_stat = {0.0, 0.0, 0.0, 0.0, 0.0}}};
 	char senderData[MAX_SIZE] = {'\0'};
 	for (int dataSetIndex = 0; dataSetIndex < NO_OF_DATASETS; dataSetIndex++ )
 	{
@@ -19,9 +18,9 @@ int main ()
 		findMovAvgOfBMSParams(&bms_input);
 		memset(senderData, '\0', MAX_SIZE);
 	}
-	printf ("Minimum values are\n----------------\n%f %f %f\n",bms_input.temperature.temp_stat.min, bms_input.soc.soc_stat.min, bms_input.chargeRate.CR_stat.min);
-	printf ("Maximum values are\n----------------\n%f %f %f\n",bms_input.temperature.temp_stat.max, bms_input.soc.soc_stat.max, bms_input.chargeRate.CR_stat.max);
-	printf ("Mov Average\n----------------\n%f %f %f\n", bms_input.temperature.temp_stat.movAvg, bms_input.soc.soc_stat.movAvg, bms_input.chargeRate.CR_stat.movAvg);
+	printf ("Minimum values are\n----------------\n%f %f\n",bms_input.temperature.temp_stat.min, bms_input.soc.soc_stat.min);
+	printf ("Maximum values are\n----------------\n%f %f\n",bms_input.temperature.temp_stat.max, bms_input.soc.soc_stat.max);
+	printf ("Mov Average\n----------------\n%f %f\n", bms_input.temperature.temp_stat.movAvg, bms_input.soc.soc_stat.movAvg);
 
 	return 0;	
 }
