d1334777bdd1e38bc4f91c858a0c6c256b277459
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-22 19:28:12 +0530

    Update DataTx.h

--- DataTx.h
+++ DataTx.h
@@ -8,7 +8,7 @@
 extern int TemperatureData[NUMBERS_OF_READINGS];
 extern int SOCData[NUMBERS_OF_READINGS];
 
-void printToConsole(int batteryData[]);
-bool readDataFromFile(FILE* filePtr, int sensorData[]);
-bool BMS_DataSender(void);
-int writeToFile(void);
+void printToConsole();
+//bool readDataFromFile(FILE* filePtr, int sensorData[]);
+//bool BMS_DataSender(void);
+//int writeToFile(void);
