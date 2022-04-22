efe20028dc4a39f143101605a6153d7ec40f81a1
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-22 19:39:56 +0530

    Update BMS_Sender.c

--- BMS_Sender.c
+++ BMS_Sender.c
@@ -7,16 +7,16 @@ int SOCData[NUMBERS_OF_READINGS];
 
 int Temp_SoC_Data()
 {
-	int i;
-  printf("Temperature Data\n");
-	for (i = 0; i < NUMBERS_OF_READINGS; i++)
-	{
-	   printf("%d\n", i);
-	}
-  printf("\nSoC Data\n");
-  for (i = 0; i < NUMBERS_OF_READINGS; i++)
-	{
-	   printf("%d\n", i/2);
-	}
+int i;
+printf("Temperature Data\n");
+for (i = 0; i < NUMBERS_OF_READINGS; i++)
+{
+printf("%d\n", i);
+}
+printf("\nSoC Data\n");
+for (i = 0; i < NUMBERS_OF_READINGS; i++)
+{
+	printf("%d\n", i/2);
+}
 return 0;
 }
